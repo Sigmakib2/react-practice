090d312898649088ee9c35230d678dee4cc86101
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2023-12-15 12:10:28 +0600

    lets test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 export default function Header() {
   return (
-    <div>
-      This is a Header
-    </div>
+    <header>
+      <div className="topnav">
+        <a className="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </header>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div>
+            <img src="https://dynamiccraft.xyz/photo/season8/spawn.png" alt="Nature" className="responsive" width="600" height="400"/>
+        </div>
+    )
+}
