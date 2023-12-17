9942cbbbf80ed7d7bc03eeee1f5e98c0a536938d
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2023-12-17 16:41:20 +0600

    Update Topnav.jsx

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -1,25 +1,29 @@
 import { Link, NavLink } from "react-router-dom"
+import { useState } from "react";
 import "./Topnav.css"
 export default function Topnav() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
-      <Link to="/" className="title">
-        Website
-      </Link>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/stats">Stats</NavLink>
-        </li>
-        <li>
-          <NavLink to ="/leaderboard">Leaderboard</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <Link to="/" className="title">
+      Website
+    </Link>
+    <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <ul className={menuOpen ? "open" : ""}>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/stats">Stats</NavLink>
+      </li>
+      <li>
+        <NavLink to="/leaderboard">LeaderBoard</NavLink>
+      </li>
+    </ul>
+  </nav>
   )
 }
