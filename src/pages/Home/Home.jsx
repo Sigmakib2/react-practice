2efec1cd3f3636690a0c52efea47bc517e560f8b
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2024-02-02 00:23:44 +0600

    api added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
+import Status from '../../components/Status/Status'
 
 export default function Home() {
   return (
     <div>
-        <Hero/>
+      <Hero />
+      <Status />
     </div>
   )
 }
