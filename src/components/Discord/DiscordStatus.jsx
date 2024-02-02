ae47c87cee830af98bb8749e769bbc3c7184ae88
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2024-02-02 11:43:13 +0600

    design changed

--- src/components/Discord/DiscordStatus.jsx
+++ src/components/Discord/DiscordStatus.jsx
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
 const DiscordStatus = () => {
-  const [guildInfo, setGuildInfo] = useState(null);
+    const [guildInfo, setGuildInfo] = useState(null);
 
-  useEffect(() => {
-    const fetchGuildInfo = async () => {
-      try {
-        const response = await fetch('https://discord.com/api/v10/invites/Dyncd6eMEy?with_counts=true&with_expiration=true');
-        const data = await response.json();
-        setGuildInfo(data);
-      } catch (error) {
-        console.error('Error fetching guild info:', error);
-      }
+    useEffect(() => {
+        const fetchGuildInfo = async () => {
+            try {
+                const response = await fetch('https://discord.com/api/v10/invites/Dyncd6eMEy?with_counts=true&with_expiration=true');
+                const data = await response.json();
+                setGuildInfo(data);
+            } catch (error) {
+                console.error('Error fetching guild info:', error);
+            }
+        };
+
+        fetchGuildInfo();
+    }, []);
+    const handleJoinDiscord = () => {
+        // Replace 'YOUR_DISCORD_INVITE_LINK' with the actual Discord invite link
+        window.location.href = 'https://discord.com/invite/Dyncd6eMEy';
     };
-
-    fetchGuildInfo();
-  }, []);
-
-  return (
-<div className='status' style={{ display: 'flex', alignItems: 'center' }}>
-      {guildInfo && (
-        <span style={{ display: 'flex', alignItems: 'center' }}>
-          <img draggable="false" src="/assets/discord.svg" alt="" style={{ marginRight: '10px' }} />
-          Active {guildInfo.approximate_presence_count} of {guildInfo.approximate_member_count} members
-        </span>
-      )}
-      <button style={{ marginLeft: '10px' }}>Join Discord</button>
-    </div>
-  );
+    return (
+        <div className='status' style={{ display: 'flex', alignItems: 'center' }}>
+            {guildInfo && (
+                <span style={{ display: 'flex', alignItems: 'center' }}>
+                    <img draggable="false" src="/assets/discord.svg" alt="" style={{ marginRight: '10px' }} />
+                    Now active {guildInfo.approximate_presence_count} of {guildInfo.approximate_member_count} members
+                </span>
+            )}
+            <button className='status' style={{ marginLeft: '10px', cursor: 'pointer' }} onClick={handleJoinDiscord}><b>Join Discord</b></button>
+        </div>
+    );
 };
 
 export default DiscordStatus;
--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -21,10 +21,11 @@ const Status = () => {
   return (
     <div className='card'>
       {serverData ? (
-        <div>
-        <span className='status'>DynamicCraft is <b>{serverData.online ? 'Online' : 'Offline'}</b> Now!</span>
-        <span className='status'>Currently playing <b>{serverData.players.now}/{serverData.players.max}</b></span>
-        <span className='status'>Can Join: <b>{serverData.players.max - serverData.players.now}</b></span>
+        <div className='server-status-container'>
+          <span className='status'>DynamicCraft is <b>{serverData.online ? 'Online' : 'Offline'}</b> Now!</span>
+          <span className='status'>Season 10 Running</span>
+          <span className='status'>Currently playing <b>{serverData.players.now}/{serverData.players.max}</b></span>
+          <span className='status'>Can Join: <b>{serverData.players.max - serverData.players.now}</b></span>
         </div>
       ) : (
         <p className='status'>Loading server data...</p>
